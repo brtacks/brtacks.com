362a2e67f465cf90f15eeb219fe6aed4e7a5f100
Jason Kao <jasonkao85@gmail.com>
2018-06-11 20:49:12 -0400

    prelim homem page

--- src/js/modules/core/components/HomePage.js
+++ src/js/modules/core/components/HomePage.js
@@ -1,24 +1,71 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import injectSheet from 'react-jss';
 
 const styles = {
-  MainApp: {
-    width: '100%',
-    height: '90vh',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    fontSize: '100px',
+  title: {
+    fontFamily: 'Domaine Display',
+    fontSize: 132,
+    '& p': {
+      lineHeight: 1,
+      position: 'absolute',
+    },
+    '& p:first-child': {
+      top: '100px',
+      left: '72px',
+    },
+    '& p:nth-child(2)': {
+      bottom: '100px',
+      left: '26vw',
+    },
+    '& p:last-child': {
+      right: '120px',
+      top: '43vh',
+    },
   },
+  navbar: {
+    fontFamily: 'Neue Haas Grotesk',
+    position: 'absolute',
+    right: '120px',
+    top: '56px',
+    '& li': {
+      listStyleType: 'none',
+      '& a': {
+        color: '#000',
+        textDecoration: 'none',
+        fontSize: '35px',
+        margin: '4px 0',
+      },
+    },
+  },
+  tagline: {
+    fontFamily: 'Neue Haas Grotesk',
+    position: 'absolute',
+    left: '25vw',
+    top: '35vh',
+    fontSize: '33px',
+    maxWidth: '220px',
+  },
+  underline: {
+    textDecoration: 'underline !important',
+  }
 };
 
-const MainApp = ({ classes }) => (
-  <div className={classes.MainApp}>
-    <p>
-      Welcome to <a href="http://github.com/brtacks">The Brass Tacks.</a>
-    </p>
+const HomePage = ({ classes }) => (
+  <div>
+    <div className={classes.title}>
+      <p>The</p>
+      <p>Brass</p>
+      <p>Tacks</p>
+    </div>
+    <ul className={classes.navbar}>
+      <li><Link className={classes.underline} to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/articles">Articles</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+    </ul>
+    <p className={classes.tagline}>Reason over rhetoric.</p>
   </div>
 );
 
-export default injectSheet(styles)(MainApp);
+export default injectSheet(styles)(HomePage);
--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.49' };
+let VersionInfo = { VERSION : '0.0.113' };
 export default VersionInfo;
