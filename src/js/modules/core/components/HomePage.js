ad042f25b263b286ec5be769661ee7b8af53b934
Jason Kao <jasonkao85@gmail.com>
2018-06-12 18:40:37 -0400

    home page

--- src/js/modules/core/components/HomePage.js
+++ src/js/modules/core/components/HomePage.js
@@ -1,91 +1,120 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import injectSheet from 'react-jss';
-import Circles from './Circles';
+import { Mail } from './Icons';
 
 const styles = {
+  HomePage: {
+    margin: '182px 14vw 0 14vw',
+  },
+  info: {
+    position: 'absolute',
+    right: '45px',
+    top: '21px',
+    textAlign: 'right',
+  },
   title: {
     fontFamily: 'Domaine Display',
-    fontSize: '132px',
-    '& p': {
-      lineHeight: 1,
-      position: 'absolute',
-      transformDuration: '.3s',
-    },
-    '& p:first-child': {
-      top: '100px',
-      left: '72px',
-    },
-    '& p:nth-child(2)': {
-      bottom: '100px',
-      left: '26vw',
-    },
-    '& p:last-child': {
-      right: '120px',
-      top: '43vh',
+    fontSize: '66px',
+    marginBottom: '28px',
+    lineHeight: 0.64,
+  },
+  subtitle: {
+    fontFamily: 'Atlas Grotesk',
+    fontSize: '22px',
+    lineHeight: 1.5,
+    width: '50%',
+  },
+  sneakPeek: {
+    marginTop: '202px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  leftCol: {
+    flexBasis: '42%',
+  },
+  header: {
+    fontFamily: 'Canela',
+    fontSize: '36px',
+    lineHeight: 1.17,
+    marginBottom: '17px',
+  },
+  preview: {
+    fontFamily: 'Atlas Grotesk',
+    fontSize: '17px',
+    lineHeight: 1.59,
+  },
+  rightCol: {
+    flexBasis: '44%',
+  },
+  imgContainer: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    boxShadow: '0 0 8px 2px rgba(136, 136, 136, 0.5)',
+    padding: '22px 0',
+    textAlign: 'center',
+    '& img': {
+      width: '90%',
+      position: 'relative',
+      top: '50%',
+      transform: 'translateY(-50%)',
     },
   },
-  navbar: {
-    fontFamily: 'Neue Haas Grotesk',
-    position: 'absolute',
-    right: '120px',
-    top: '56px',
-    '& li': {
-      listStyleType: 'none',
-      '& a': {
-        color: '#000',
-        textDecoration: 'none',
-        fontSize: '32px',
-        margin: '6px 0',
-      },
+  email: {
+    fontFamily: 'Maison Neue',
+    fontSize: '16px',
+    marginBottom: '9px',
+    '& a': {
+      color: '#000',
+      textDecoration: 'none',
+    },
+    '& img': {
+      marginRight: '6px',
+      position: 'relative',
+      top: '3px',
     },
   },
-  tagline: {
-    fontFamily: 'Neue Haas Grotesk',
-    position: 'absolute',
-    left: '25vw',
-    top: '35vh',
-    fontSize: '46px',
-    maxWidth: '280px',
-  },
-  underline: {
-    textDecoration: 'underline !important',
-  },
-  '@media (max-width: 991px)': {
-    title: {
-      fontSize: '5em',
-    },
-    tagline: {
-      fontSize: '32px',
-    },
+  iLoveNyc: {
+    fontSize: '13px',
+    fontFamily: 'Atlas Grotesk',
+    color: 'rgba(0, 0, 0, 0.82)',
+    textTransform: 'uppercase',
   },
 };
 
 const HomePage = ({ classes }) => (
-  <div>
-    <Circles />
-    <div className={classes.title}>
-      <p>The</p>
-      <p>Brass</p>
-      <p>Tacks</p>
+  <div className={classes.HomePage}>
+    <div className={classes.info}>
+      <p className={classes.email}>
+        <Mail />
+        <Link to="mailto:jason.kao@columbia.edu">jason.kao@columbia.edu</Link>
+      </p>
+      <p className={classes.iLoveNyc}>
+        Made in New York, NY
+      </p>
     </div>
-    <ul className={classes.navbar}>
-      <li>
-        <Link className={classes.underline} to="/">
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/articles">Articles</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
-    <p className={classes.tagline}>Reason over rhetoric.</p>
+
+    <p className={classes.title}>The Brass Tacks</p>
+    <p className={classes.subtitle}>
+      We publish visual essays on the web to inform sociopolitical discussion.
+    </p>
+
+    <div className={classes.sneakPeek}>
+      <div className={classes.leftCol}>
+        <p className={classes.header}>
+          Our first article is around the corner.
+        </p>
+        <p className={classes.preview}>
+          Titled “The Honorable Handshakers,” this piece analyzes the methods and extent to which the Democrats and the Republicans have appealed to the morals within us.
+        </p>
+      </div>
+      <div className={classes.rightCol}>
+        <div className={classes.imgContainer}>
+          <img src="./img/lyrics.jpg" />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.153' };
+let VersionInfo = { VERSION : '0.0.257' };
 export default VersionInfo;
